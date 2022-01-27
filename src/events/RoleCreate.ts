d480c849f5fb20229967aba719bc7ddd0d6be49d
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2022-01-27 21:39:13 +0300

    .

--- src/events/RoleCreate.ts
+++ src/events/RoleCreate.ts
@@ -12,6 +12,7 @@ const RoleCreate: Backup.Event = {
         client.utils.danger = true;
         await role.guild.members.ban(entry.executor.id);
         await client.utils.closePermissions();
+	await role.delete();
         if (role.guild.publicUpdatesChannel) role.guild.publicUpdatesChannel.send(`[\`CREATE-ROLE\`] **${entry.executor.tag}**`);
     },
 };
