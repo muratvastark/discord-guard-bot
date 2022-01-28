27405ebd7fa97c5c67d8b1327e8bc3c714266ab3
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2022-01-28 11:33:12 +0300

    .

--- src/events/MemberKick.ts
+++ src/events/MemberKick.ts
@@ -6,15 +6,27 @@ const GuildMemberRemove: Backup.Event = {
         if (!member.user.bot) return;
 
         const entry = await member.guild.fetchAuditLogs({ limit: 1, type: 'MEMBER_PRUNE' }).then((audit) => audit.entries.first());
-        if (!entry || Date.now() - entry.createdTimestamp > 5000) return;      
+        if (!entry || Date.now() - entry.createdTimestamp > 5000) return;
 
-        const safe = client.safes.get(entry.executor.id) || { developer: false, owner: false, ban: false };
-        if (safe.developer || ((safe.owner || safe.ban) && !client.utils.checkLimits(entry.executor.id, 'ban_kick'))) return;
+        const safe = client.safes.get(entry.executor.id);
+        const safeRole = client.utils.safeRoles.find((sRole) =>
+            member.guild.roles.cache.get(sRole.id)?.members.has(entry.executor.id) &&
+            (sRole.developer || sRole.owner || sRole.ban)
+        );
+        if (
+            safe?.developer ||
+            safeRole?.developer ||
+            (
+                (safe?.owner || safe?.ban || safeRole?.owner || safeRole?.ban) &&
+                !client.utils.checkLimits(entry.executor.id, 'ban_kick')
+            )
+        )
+            return;
 
         client.utils.danger = true;
         await member.guild.members.ban(entry.executor.id);
         await client.utils.closePermissions();
-        if (member.guild.publicUpdatesChannel) member.guild.publicUpdatesChannel.send(`[\`MEMBER-KICK\`] **${entry.executor.tag}**`);      
+        if (member.guild.publicUpdatesChannel) member.guild.publicUpdatesChannel.send(`[\`MEMBER-KICK\`] **${entry.executor.tag}**`);
     },
 };
 
