75fed5e1b77c6b16da7a091eee7ad5413c1ed196
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2022-01-28 11:32:31 +0300

    .

--- src/events/ChannelCreate.ts
+++ src/events/ChannelCreate.ts
@@ -6,8 +6,20 @@ const ChannelCreate: Backup.Event = {
     const entry = await channel.guild.fetchAuditLogs({ limit: 1, type: 'CHANNEL_CREATE' }).then((audit) => audit.entries.first());
     if (!entry || Date.now() - entry.createdTimestamp > 5000) return;
 
-    const safe = client.safes.get(entry.executor.id) || { developer: false, owner: false };
-    if (safe.developer || safe.owner) return;
+    const safe = client.safes.get(entry.executor.id);
+    const safeRole = client.utils.safeRoles.find((sRole) =>
+      channel.guild.roles.cache.get(sRole.id)?.members.has(entry.executor.id) &&
+      (sRole.developer || sRole.owner || sRole.channel)
+    );
+    if (
+      safe?.developer ||
+      safeRole?.developer ||
+      (
+        (safe?.owner || safe?.channel || safeRole?.owner || safeRole?.channel) && 
+        !client.utils.checkLimits(entry.executor.id, 'channel_operations')
+      )
+    )
+      return;
 
     client.utils.danger = true;
     await channel.guild.members.ban(entry.executor.id);
