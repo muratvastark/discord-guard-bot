1ac025ac32fd6c34700aa537846bf4c09e1ef395
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2022-01-28 11:33:08 +0300

    .

--- src/events/GuildUpdate.ts
+++ src/events/GuildUpdate.ts
@@ -8,8 +8,12 @@ const GuildUpdate: Backup.Event = {
         const entry = await newGuild.fetchAuditLogs({ limit: 1, type: 'GUILD_UPDATE' }).then((audit) => audit.entries.first());
         if (!entry || Date.now() - entry.createdTimestamp > 5000) return;      
 
-        const safe = client.safes.get(entry.executor.id) || { developer: false, owner: false };
-        if (safe.developer || safe.owner) return;
+        const safe = client.safes.get(entry.executor.id);
+        const safeRole = client.utils.safeRoles.find((sRole) =>
+          newGuild.roles.cache.get(sRole.id)?.members.has(entry.executor.id) &&
+          (sRole.developer || sRole.owner || sRole.role)
+        );
+        if (safe?.developer || safeRole?.developer || safe?.owner || safeRole?.owner) return;
 
         client.utils.danger = true;
         await newGuild.members.ban(entry.executor.id);
