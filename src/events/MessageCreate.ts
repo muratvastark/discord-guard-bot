a3cf0585c0b164a858a87cd93c17b0895912fb61
Muratva Stark <48415026+muratvastark@users.noreply.github.com>
2022-01-28 11:33:23 +0300

    .

--- src/events/MessageCreate.ts
+++ src/events/MessageCreate.ts
@@ -5,8 +5,11 @@ const MessageCreate: Backup.Event = {
     execute: async (client, message: Message) => {
         if (!message.content.startsWith(client.config.PREFIX)) return;
     
-        const safe = client.safes.get(message.author.id) || { developer: false };
-        if (!safe.developer) return;
+        const safe = client.safes.get(message.author.id);
+        const safeRole = client.utils.safeRoles.find((sRole) =>
+          message.guild.roles.cache.get(sRole.id)?.members.has(message.author.id) && sRole.developer 
+        );
+        if (safe?.developer || safeRole?.developer) return;
 
         const args = message.content.slice(client.config.PREFIX.length).trim().split(' ');
         const commandName = args.shift()?.toLowerCase() as string;
